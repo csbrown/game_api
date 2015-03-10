4fd89f3642df60eac3be65081df6b69de4f33aa6
scott <gitpushoriginmaster@gmail.com>
2015-03-10 14:26:17 -0400

    added stat model to games, and updated api urls

--- game_server/settings.py
+++ game_server/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'games',
 )
 
 MIDDLEWARE_CLASSES = (
--- games/models.py
+++ games/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Stat(models.Model):
+	user = models.ForeignKey(User)
+	game_engine = models.CharField(max_length = 60)
+	wins = models.IntegerField(default = 0)
+	losses = models.IntegerField(default = 0)
+	ties = models.IntegerField(default = 0)
+
+	def total_games(self):
+		return self.wins + self.ties + self.losses
+
+	def win_ratio(self):
+		if self.total_games() == 0:
+			return 0
+		return self.wins*1./self.total_games()
+
+	def loss_ratio(self):
+		if self.total_games() == 0:
+			return 0
+		return self.losses*1./self.total_games()
