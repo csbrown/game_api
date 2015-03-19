46a2b0b5e8573b83af0a5833c40a313d2328f47d
scott <gitpushoriginmaster@gmail.com>
2015-03-18 22:37:27 -0400

    finished tictactoe game front end

--- games/views.py
+++ games/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader, Context
 
-# Create your views here.
+def tictactoe(request):
+    template = loader.get_template('games/tictactoe.html')
+    return HttpResponse(template.render(Context()))
+
+def index(request):
+    return HttpResponse("We're still working on this... Hold tight")
